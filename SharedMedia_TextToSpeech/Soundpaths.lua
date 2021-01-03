cbe1478d39c69732d8b743e6047bf5c919e9f20d
Brandon Pierce <brandonpierce91@gmail.com>
2021-01-03 13:58:39 -0500

    Port over english mage text to speech

--- SharedMedia_TextToSpeech/Soundpaths.lua
+++ SharedMedia_TextToSpeech/Soundpaths.lua
@@ -2,7 +2,25 @@ local LSM = LibStub("LibSharedMedia-3.0")
 
 -- -----
 --   SOUND
+--   Generated via: https://ttsmp3.com/
 -- -----
+
+-- -----
+--   US English / Salli
+-- -----
+
+-- Mage
+LSM:Register("sound", "|cFF3FC7EBCombustion|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_combustion.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBRune of Power|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_rune_of_power.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBShifting Power|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_shifting_power.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBAlter Time|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_alter_time.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBFrost Nova|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_frost_nova.mp3]]) 
+
+-- -----
+--   German / Marlene
+-- -----
+
+-- Warlock
 LSM:Register("sound", "|cFF8788EETextToSpeech:German Agony|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_agony.mp3]])
 LSM:Register("sound", "|cFF8788EETextToSpeech:German Corruption|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_corruption.mp3]])
 LSM:Register("sound", "|cFF8788EETextToSpeech:German Shadow Embrace|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_embrace.mp3]])
