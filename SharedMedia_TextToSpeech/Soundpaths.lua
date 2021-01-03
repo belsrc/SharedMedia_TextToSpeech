2a16352b21e21f8e06027fb32c2b4834bc86c381
Bryan Kizer <brykizer@gmail.com>
2021-01-03 00:03:03 -0500

    chore: initial commit

---
+++ SharedMedia_TextToSpeech/Soundpaths.lua
@@ -0,0 +1,13 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Agony|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_agony.mp3]])
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Corruption|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_corruption.mp3]])
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Shadow Embrace|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_embrace.mp3]])
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Haunt|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_haunt.mp3]])
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Siphon Life|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_siphon.mp3]])
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Unstable Affliction|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_unstable.mp3]])
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Darkglare|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_darkglare.mp3]])
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Dark Soul Misery|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_misery.mp3]])
