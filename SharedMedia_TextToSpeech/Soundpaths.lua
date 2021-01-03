15f768b2dcf097244e22e4400bf94c1dc63bf585
Bryan Kizer <brykizer@gmail.com>
2021-01-03 00:16:41 -0500

    feat: add Soul Rot

--- SharedMedia_TextToSpeech/Soundpaths.lua
+++ SharedMedia_TextToSpeech/Soundpaths.lua
@@ -11,3 +11,4 @@ LSM:Register("sound", "|cFF8788EETextToSpeech:German Siphon Life|r", [[Interface
 LSM:Register("sound", "|cFF8788EETextToSpeech:German Unstable Affliction|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_unstable.mp3]])
 LSM:Register("sound", "|cFF8788EETextToSpeech:German Darkglare|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_darkglare.mp3]])
 LSM:Register("sound", "|cFF8788EETextToSpeech:German Dark Soul Misery|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_misery.mp3]])
+LSM:Register("sound", "|cFF8788EETextToSpeech:German Soul Rot|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_soulrot.mp3]])
