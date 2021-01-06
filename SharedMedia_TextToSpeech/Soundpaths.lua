82c58cdced765f9f2b080f00e7f3f1e8babccaa3
Brandon Pierce <brandonpierce91@gmail.com>
2021-01-05 19:24:33 -0500

    Update folder structure and filenames

--- SharedMedia_TextToSpeech/Soundpaths.lua
+++ SharedMedia_TextToSpeech/Soundpaths.lua
@@ -10,23 +10,23 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 
 -- Mage
-LSM:Register("sound", "|cFF3FC7EBTTS > English > Combustion|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_combustion.mp3]]) 
-LSM:Register("sound", "|cFF3FC7EBTTS > English > Rune of Power|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_rune_of_power.mp3]]) 
-LSM:Register("sound", "|cFF3FC7EBTTS > English > Shifting Power|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_shifting_power.mp3]]) 
-LSM:Register("sound", "|cFF3FC7EBTTS > English > Alter Time|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_alter_time.mp3]]) 
-LSM:Register("sound", "|cFF3FC7EBTTS > English > Frost Nova|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english_frost_nova.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBTTS : Eng : Combustion|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english\mage\combustion.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBTTS : Eng : Rune of Power|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english\mage\rune_of_power.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBTTS : Eng : Shifting Power|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english\mage\shifting_power.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBTTS : Eng : Alter Time|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english\mage\alter_time.mp3]]) 
+LSM:Register("sound", "|cFF3FC7EBTTS : Eng : Frost Nova|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\english\mage\frost_nova.mp3]]) 
 
 -- -----
 --   German / Marlene
 -- -----
 
 -- Warlock
-LSM:Register("sound", "|cFF8788EETTS > German > Agony|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_agony.mp3]])
-LSM:Register("sound", "|cFF8788EETTS > German > Corruption|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_corruption.mp3]])
-LSM:Register("sound", "|cFF8788EETTS > German > Shadow Embrace|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_embrace.mp3]])
-LSM:Register("sound", "|cFF8788EETTS > German > Haunt|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_haunt.mp3]])
-LSM:Register("sound", "|cFF8788EETTS > German > Siphon Life|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_siphon.mp3]])
-LSM:Register("sound", "|cFF8788EETTS > German > Unstable Affliction|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_unstable.mp3]])
-LSM:Register("sound", "|cFF8788EETTS > German > Darkglare|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_darkglare.mp3]])
-LSM:Register("sound", "|cFF8788EETTS > German > Dark Soul Misery|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_misery.mp3]])
-LSM:Register("sound", "|cFF8788EETTS > German > Soul Rot|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german_soulrot.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Agony|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlockagony.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Corruption|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlockcorruption.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Shadow Embrace|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlockembrace.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Haunt|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlockhaunt.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Siphon Life|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlocksiphon.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Unstable Affliction|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlockunstable.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Darkglare|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlockdarkglare.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Dark Soul Misery|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlockmisery.mp3]])
+LSM:Register("sound", "|cFF8788EETTS : Ger : Soul Rot|r", [[Interface\Addons\SharedMedia_TextToSpeech\sound\german\warlocksoulrot.mp3]])
